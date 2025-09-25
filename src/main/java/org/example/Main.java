e7505343a673f49bf1b36386c565c19d4c382d81
madhav8511 <mgirdhar027@gmail.com>
2025-09-25 18:35:51 +0530

    Automation checking

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
-
+        System.out.println("Checking if Build is automatically intiated or not");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
