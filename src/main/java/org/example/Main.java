81ddc3b059a63f703a1a960e99ce137195e05c14
madhav8511 <mgirdhar027@gmail.com>
2025-09-25 22:05:37 +0530

    Webhook testing v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("Checking if Build is automatically intiated or not");
-        System.out.println("Webhook testing - 1");
+        System.out.println("Webhook testing - 12");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
